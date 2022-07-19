2fc009cd9a690abb0144d5ce5a1de635caee6c4a
Nilgam <nilgam@proton.me>
2022-07-19 17:09:05 +0200

    Fix navigation.css naming

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,11 @@
-import {NavLink} from "react-router-dom";
-import '../styles/Navigation.css'
+import { NavLink } from "react-router-dom";
+import "../styles/navigation.css";
 
 const Navigation = () => (
-    <nav className="main-nav">
-        <NavLink to="/">Accueil</NavLink>
-        <NavLink to="/login">Login</NavLink>
-    </nav>
-)
+  <nav className="main-nav">
+    <NavLink to="/">Accueil</NavLink>
+    <NavLink to="/login">Login</NavLink>
+  </nav>
+);
 
 export default Navigation;
