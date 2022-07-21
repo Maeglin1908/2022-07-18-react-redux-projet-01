4443f993b39504593b067fee283027ce851bcee8
Nilgam <benjamin.thiery1908@gmail.com>
2022-07-21 22:30:18 +0200

    Partie 4 - Store games with api into json db

---
+++ src/utils/services/GameService.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+const url = "http://localhost:6789/games";
+
+export function getAll() {
+    return axios
+        .get(url)
+        .then((res) => res.data)
+        .catch((err) => alert("Nope (get all)"));
+}
+
+// Avec async, juste pour avoir les deux façons de faire.
+export async function getById(id) {
+    try {
+        const { data } = await axios.get(`${url}/${id}`);
+        return data;
+    } catch {
+        alert("Nope (get by id)");
+    }
+}
+
+export function getByTitle(title) {
+    return axios
+        .get(url, {
+            params: { title },
+        })
+        .then((res) => res.data)
+        .catch(() => alert("C'est cassé (get by title)"));
+}
+
+export function getByStudio(studio) {
+    return axios
+        .get(url, {
+            params: { studio },
+        })
+        .then((res) => res.data)
+        .catch(() => alert("C'est cassé (get by studio)"));
+}
+
+export function create(game) {
+    return axios
+        .post(url, game)
+        .then((res) => res.data)
+        .catch(() => alert("C'est cassé (create)"));
+}
+
+export function update(game) {
+    return axios
+        .put(`${url}/${game.id}`, game)
+        .then((res) => res.data)
+        .catch(() => alert("C'est cassé (update)"));
+}
+
+export function remove(game) {
+    const id = typeof game === "number" ? game : game.id;
+    return axios
+        .delete(`${url}/${id}`)
+        .then(() => true)
+        .catch(() => alert("C'est cassé (delete)"));
+}
